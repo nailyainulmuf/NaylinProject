81ba0c6e9daaee0c60dee12bb324039959daaec9
WINDOWS10\WINDOWS 10 <nailyainulmuf@gmail.com>
2018-06-30 20:03:27 +0700

    penambahan models data foto pada admin

---
+++ application/models/foto_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Foto_model extends CI_Model{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function getTampilFoto()
+	{
+		$query = $this->db->query("Select * from user as u inner join foto as f on u.id_user = f.fk_user inner join kategori as k on k.id_kategori= f.id_kategori");
+		return $query->result_array();
+	}
+
+	public function insertFoto(){
+		$data = array ('judul'=>$this->input->post('judul'),'id_kategori'=>$this->input->post('kategori'), 'deskripsi'=>$this->input->post('deskripsi'), 'photo'=>$this->upload->data('file_name'), 'fk_user' =>$this->input->post('fk_user'));
+		$this->db->insert('foto', $data);
+	}
+
+	public function getFoto($id){
+		$this->db->where('id_foto', $id);
+		$query=$this->db->get('foto');
+		return $query->result();
+	}
+
+	public function getKategori()
+	{
+		$query = $this->db->query("Select * from kategori");
+		return $query->result_array();
+
+	}
+
+}
