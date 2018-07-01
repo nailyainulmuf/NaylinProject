7f7099e6f4121fe3a506290c0cc9633e2252f00e
WINDOWS10\WINDOWS 10 <nailyainulmuf@gmail.com>
2018-07-01 10:59:20 +0700

    edit tampilan pada view dashboard dan foto

--- application/views/master_admin.php
+++ application/views/master_admin.php
@@ -45,7 +45,8 @@
                     <!-- Main menu -->
                     <li class="current"><a href="<?php echo base_url('index.php/m_admin') ?>"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
                                         <li class="submenu">
-                    <li class="#"><a href="<?php echo base_url('index.php/admin_foto') ?>"><i class="glyphicon glyphicon-list"></i> Foto</a></li>
+                    <li class="#"><a href="<?php echo base_url('index.php/admin_foto') ?>"><i class="glyphicon glyphicon-picture"></i> Foto</a></li>
+                    <li><a href="#"><i class="glyphicon glyphicon-list"></i> Tips</a></li>
                     <li><a href="#"><i class="glyphicon glyphicon-list-alt"></i> Komentar</a></li>
                     <li><a href="#"><i class="glyphicon glyphicon-user"></i> User</a></li>  
                     </li>
